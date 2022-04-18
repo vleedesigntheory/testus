0dd742ca6c273cafc9994582a5b92e46a177a5d3
李恒谦 <lihengqian@lihengqiandeMacBook-Air.local>
2022-04-18 16:11:59 +0800

    test: 测试样例

---
+++ examples/example1/src/a/1.js
@@ -0,0 +1,37 @@
+/**
+ * @testus 
+ * @name sum1
+ * @description 测试sum1函数
+ * @param a 1
+ * @param b 2
+ * @return 3
+ * @testus
+ */
+const sum1 = (a,b) => a+b;
+
+/**
+ * @testus 
+ * @name sum2
+ * @description 测试sum1函数
+ * @param a 3
+ * @param b 4
+ * @return 7
+ * @testus
+ */
+ const sum2 = (a,b) => a+b;
+
+ /**
+ * @description 测试sum1函数
+ * @param a 5
+ * @param b 6
+ */
+  const sum3 = (a,b) => a+b;
+
+/**
+ * @end
+ */
+module.exports = {
+    sum1,
+    sum2,
+    sum3
+}
---
+++ examples/example1/src/app.js
@@ -0,0 +1,17 @@
+console.log('app')
+
+/**
+ * @testus 
+ * @name app
+ * @description 测试app
+ * @return 'app'
+ * @testus
+ */
+const app = () => ['a', 'p', 'p'].join('');
+
+/**
+ * @end
+ */
+module.exports = {
+    app
+}
